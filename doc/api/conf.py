ce959f6338514a36ff8399a2071ba70835477137
Dev Ahluwalia <devahluwalia34@gmail.com>
2025-07-10 17:31:55 -0500

    readthedocs theme

--- doc/api/conf.py
+++ doc/api/conf.py
@@ -20,6 +20,7 @@
 import os
 import sys
 import numpydoc
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../..'))
 
@@ -35,7 +36,7 @@ print(sys.executable)
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'numpydoc']
+extensions = ['sphinx.ext.autodoc', 'numpydoc', 'sphinx_rtd_theme',]
 
 
 # Add any paths that contain templates here, relative to this directory.
@@ -88,8 +89,9 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'groundwork'
-
+#html_theme = 'groundwork'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
