c43dd9a6357d73fbb3fec3db812b36dd05231365
Dev Ahluwalia <devahluwalia34@gmail.com>
2025-11-25 17:07:09 -0500

    fix progress bar issue, create test suite (in progress)

---
+++ tests/test_query_spec.py
@@ -0,0 +1,78 @@
+"""Unit tests for sparkly.query_generator.query_spec module."""
+
+import pytest
+from sparkly.query_generator import query_spec
+
+
+class TestQuerySpec:
+    """Tests for QuerySpec class."""
+
+    def test_query_spec_init_from_dict(self):
+        """Test QuerySpec initialization from dictionary."""
+        # TODO: Implement test
+        pass
+
+    def test_query_spec_init_from_kwargs(self):
+        """Test QuerySpec initialization from keyword arguments."""
+        # TODO: Implement test
+        pass
+
+    def test_query_spec_init_validation(self):
+        """Test QuerySpec initialization validates value types."""
+        # TODO: Implement test - should raise TypeError for invalid types
+        pass
+
+    def test_query_spec_dict_behavior(self):
+        """Test QuerySpec dict-like behavior (getitem, setitem, etc.)."""
+        # TODO: Implement test
+        pass
+
+    def test_query_spec_size_property(self):
+        """Test QuerySpec size property (computes sum of all path lengths)."""
+        # TODO: Implement test
+        pass
+
+    def test_query_spec_filter_property(self):
+        """Test QuerySpec filter property getter and setter."""
+        # TODO: Implement test
+        pass
+
+    def test_query_spec_filter_setter_validation(self):
+        """Test QuerySpec filter setter validates input."""
+        # TODO: Implement test - should raise TypeError/RuntimeError for invalid input
+        pass
+
+    def test_query_spec_boost_map_property(self):
+        """Test QuerySpec boost_map property getter and setter."""
+        # TODO: Implement test
+        pass
+
+    def test_query_spec_boost_map_setter_validation(self):
+        """Test QuerySpec boost_map setter validates input."""
+        # TODO: Implement test - should raise TypeError for invalid input
+        pass
+
+    def test_query_spec_union(self):
+        """Test QuerySpec union method."""
+        # TODO: Implement test
+        pass
+
+    def test_query_spec_is_subset(self):
+        """Test QuerySpec is_subset method."""
+        # TODO: Implement test
+        pass
+
+    def test_query_spec_to_dict(self):
+        """Test QuerySpec to_dict method."""
+        # TODO: Implement test
+        pass
+
+    def test_query_spec_hash(self):
+        """Test QuerySpec __hash__ method."""
+        # TODO: Implement test
+        pass
+
+    def test_query_spec_eq(self):
+        """Test QuerySpec __eq__ method."""
+        # TODO: Implement test
+        pass
---
+++ tests/test_search.py
@@ -0,0 +1,62 @@
+"""Unit tests for sparkly.search module."""
+
+import pytest
+from sparkly import search
+
+
+class TestSearcher:
+    """Tests for Searcher class."""
+
+    def test_searcher_init_defaults(self):
+        """Test Searcher initialization with default parameters."""
+        # TODO: Implement test
+        pass
+
+    def test_searcher_init_custom_chunk_size(self):
+        """Test Searcher initialization with custom search_chunk_size."""
+        # TODO: Implement test
+        pass
+
+    def test_searcher_init_validation(self):
+        """Test Searcher parameter validation (type checking)."""
+        # TODO: Implement test
+        pass
+
+    def test_searcher_get_full_query_spec(self):
+        """Test Searcher get_full_query_spec method (delegates to index)."""
+        # TODO: Implement test
+        pass
+
+    def test_searcher_search(self):
+        """Test Searcher search method."""
+        # TODO: Implement test
+        pass
+
+    def test_searcher_search_gen(self):
+        """Test Searcher search_gen method."""
+        # TODO: Implement test
+        pass
+
+
+class TestSearchFunctions:
+    """Tests for module-level search functions."""
+
+    def test_search_spark(self):
+        """Test _search_spark function."""
+        # TODO: Implement test
+        pass
+
+    def test_search_many(self):
+        """Test _search_many function."""
+        # TODO: Implement test
+        pass
+
+    def test_search(self):
+        """Test search function."""
+        # TODO: Implement test
+        pass
+
+    def test_search_gen(self):
+        """Test search_gen function."""
+        # TODO: Implement test
+        pass
