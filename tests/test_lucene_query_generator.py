8a1f135f402bfa0a1bb124883089b4955b1f6b7c
Dev Ahluwalia <devahluwalia34@gmail.com>
2025-11-25 17:07:09 -0500

    fix progress bar issue, create test suite (in progress)

---
+++ tests/test_lucene_query_generator.py
@@ -0,0 +1,23 @@
+"""Unit tests for sparkly.query_generator.lucene_query_generator module."""
+
+import pytest
+from sparkly.query_generator import lucene_query_generator
+
+
+class TestLuceneQueryGenerator:
+    """Tests for LuceneQueryGenerator class."""
+
+    def test_lucene_query_generator_init(self):
+        """Test LuceneQueryGenerator initialization with analyzer, config, and index_reader."""
+        # TODO: Implement test
+        pass
+
+    def test_lucene_query_generator_generate(self):
+        """Test LuceneQueryGenerator generate method."""
+        # TODO: Implement test
+        pass
+
+    def test_lucene_query_generator_generate_from_spec(self):
+        """Test LuceneQueryGenerator generate_from_spec method."""
+        # TODO: Implement test
+        pass
---
+++ tests/test_thread_progress_bar.py
@@ -0,0 +1,33 @@
+"""Unit tests for sparkly.thread_progress_bar module."""
+
+import pytest
+from sparkly import thread_progress_bar
+
+
+class TestThreadProgressBar:
+    """Tests for ThreadProgressBar class."""
+
+    def test_thread_progress_bar_init(self):
+        """Test ThreadProgressBar initialization with parameters."""
+        # TODO: Implement test
+        pass
+
+    def test_thread_progress_bar_start(self):
+        """Test ThreadProgressBar start method."""
+        # TODO: Implement test
+        pass
+
+    def test_thread_progress_bar_stop(self):
+        """Test ThreadProgressBar stop method."""
+        # TODO: Implement test
+        pass
+
+    def test_thread_progress_bar_update(self):
+        """Test ThreadProgressBar update method."""
+        # TODO: Implement test
+        pass
+
+    def test_thread_progress_bar_context_manager(self):
+        """Test ThreadProgressBar as context manager."""
+        # TODO: Implement test
+        pass
